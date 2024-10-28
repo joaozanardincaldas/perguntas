ce8f684e51e9a1ae3fd2f5c5f29528aa863a265c
joaozanardincaldas <145141350+joaozanardincaldas@users.noreply.github.com>
2024-10-28 11:48:44 -0300

    Add files via upload

---
+++ João Zanar/perguntas.js
@@ -0,0 +1,51 @@
+criaCartao(
+    'Quem sou eu?',
+    '',
+    'eu sou o Matheus Jesus, tenho 16 anos e sou multi atleta'
+)
+
+criaCartao(
+    'Minha familia é?',
+    '',
+    'Muito unida e divertida'
+)
+
+criaCartao(
+    'Onde moro?',
+    '',
+    'Moro em Pinhãp-PR no bairro Lindouro, longitude-25.691931, latitude-51.653184'
+)
+
+criaCartao(
+    'Onde estudo?',
+    '',
+    'Estudo no Colegio Estadual Prof. Mario Evaldo Morski fundado em 1987'
+)
+
+
+criaCartao(
+    'Minha turma?',
+    '',
+    'Loucos, divertidos, legais e muitas das vezes insuportaveis'
+)
+
+
+criaCartao(
+    'Faculdade?',
+    '',
+    'Planejo me formar em Engenharia e Arquitetura'
+)
+
+
+criaCartao(
+    'Minha cidade?',
+    '',
+    'Uma cidade pouco desenvolvida, porem é um otimo cantinho de paz'
+)
+
+
+criaCartao(
+    'Meu sonho?',
+    '',
+    'Construir uma familia ao lado de uma mulher incrivel e brilhante '
+)
